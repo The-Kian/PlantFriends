8fdc8cc6315334f49a666f30ad05603723e0b74e
the-kian <kian.popat@flutteruki.com>
2024-09-13 18:56:17 +0100

    init

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+
+  return (
+    <ThemedView    >
+      <ThemedText>Edit app/index.tsx to edit this screen.</ThemedText>
+    </ThemedView>
+  );
+}
+
