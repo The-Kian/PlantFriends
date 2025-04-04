7adf6dd4d525741276adb5d94b51b8410a0f083b
kian-SBG <kian.popat@flutteruki.com>
2025-04-04 13:59:19 +0100

    wip

--- App.tsx
+++ App.tsx
@@ -3,21 +3,28 @@ import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 
 import { SafeAreaView } from "react-native";
+import { Provider } from 'react-redux';
+
 
 import RootLayout from "@components/navigation/RootLayout";
 import { AuthProvider } from "@context/auth/AuthProvider";
-
-
+import { store } from "@store/store";
 import './gesture-handler';
+import TestComponent from "@store/testRedux";
+
+
 
 
 export default function App() {
   return (
-    <AuthProvider>
+    <Provider store={store}>
+    {/* <AuthProvider>
       <NavigationContainer>
         <SafeAreaView />
         <RootLayout />
       </NavigationContainer>
-    </AuthProvider>
+    </AuthProvider> */}
+      <TestComponent />
+    </Provider>
   );
 }
