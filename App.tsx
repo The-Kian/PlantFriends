218f7fc9096b28ac9b7ed7d56776bbb8bdd8db6c
kian-SBG <kian.popat@flutteruki.com>
2025-04-08 18:00:43 +0100

    Redux Demo

--- App.tsx
+++ App.tsx
@@ -17,12 +17,13 @@ import TestComponent from "@store/testRedux";
 export default function App() {
   return (
     <Provider store={store}>
-    <AuthProvider>
+    {/* <AuthProvider>
       <NavigationContainer>
         <SafeAreaView />
         <RootLayout />
       </NavigationContainer>
-    </AuthProvider>
+    </AuthProvider> */}
+      <TestComponent />
     </Provider>
   );
 }
