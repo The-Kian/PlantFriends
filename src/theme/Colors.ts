7a620877634d2fd8db9744ce6ee8bf9a11ec2b8b
kian-SBG <kian.popat@skybettingandgaming.com>
2025-07-28 14:26:45 +0100

    PlantCard created

--- src/theme/Colors.ts
+++ src/theme/Colors.ts
@@ -1,4 +1,5 @@
 
+<<<<<<< HEAD
 const tintColorLight = "#0a7ea4";
 const tintColorDark = "#fff";
 
@@ -30,5 +31,43 @@ export const Colors = {
     border: "#39424E",
     greenButton: "#00A86B",
     redButton: "#FF0000",
+=======
+
+const primaryGreen = '#4A5D50';
+const accentYellow = '#E5C95F';
+const lightText = '#FFFFFF';
+const cardBackground = '#637C6A';
+const inactiveGray = '#E0E0E0';
+const errorRed = '#FF5252';
+const subtleBorder = '#7C8B7E';
+
+
+export const Colors = {
+  light: {
+    text: lightText,
+    background: primaryGreen,
+    tint: accentYellow,
+    icon: lightText,
+    tabIconDefault: inactiveGray,
+    tabIconSelected: accentYellow,
+    headerBackground: primaryGreen,
+    card: cardBackground,
+    button: accentYellow,
+    border: subtleBorder,
+    error: errorRed,
+  },
+  dark: {
+    text: lightText,
+    background: primaryGreen,
+    tint: accentYellow,
+    icon: lightText,
+    tabIconDefault: inactiveGray,
+    tabIconSelected: accentYellow,
+    headerBackground: primaryGreen,
+    card: cardBackground,
+    button: accentYellow,
+    border: subtleBorder,
+    error: errorRed,
+>>>>>>> 5eae45e (PlantCard created)
   },
 };
